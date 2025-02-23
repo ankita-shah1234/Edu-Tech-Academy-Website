93aec14c99a5938e10b6cc7796bd859e10f96966
ankita-shah1234 <ankitashah2025@gmail.com>
2025-02-23 19:59:37 +0530

    add

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import './App.css'
+import Login from './pages/Login'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+function App(){
+  return{
+    <main>
+      <Login/>
+    </main>
+  }
 }
 
 export default App
